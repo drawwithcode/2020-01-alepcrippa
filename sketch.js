afab2164132102d4134f558f00a7b85fdfc79b8d
unknown <ale.crippa96@gmail.com>
2020-09-28 14:37:31 +0200

    fixed proportions and dimensions

--- sketch.js
+++ sketch.js
@@ -13,6 +13,7 @@ let w;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  print(windowWidth, windowHeight);
   angleMode(RADIANS);
   rAngle=1;
 }
@@ -27,7 +28,7 @@ function draw() {
   waveS=100*sin(frameCount/200); //needed only for w
   wave = 100*sin(frameCount/150); //needed for the two single lines and the circle
   wave1=100*sin(frameCount/150 +PI/2); //needed for the two squares
-  w=waveS*map(width/2-60,0,height,2500,0); //just empirically found, needed for the gray lines
+  w=waveS*9 //just empirically found, needed for the gray lines width/2-60
   i=map(sin(frameCount/200-PI),-1,1,0,1); //needed for the colors
 
   color1=color(255-int(i*128),8+int(i*247),90+int(i*122));
@@ -37,8 +38,8 @@ function draw() {
   rotate(wave1/50);
   color1.setAlpha(255)
   stroke(color1);
-  stroke(lerpColor(color("red"), color("blue"), frameCount/120));
-  rect(-wave1*3/2,-wave1*3/2,2*wave1*3/2,2*3/2*wave1);
+  //stroke(lerpColor(color("red"), color("blue"), frameCount/120));
+  rect(-wave1*height/400,-wave1*height/400,2*wave1*height/400,2*height/400*wave1);
   pop();
 
   history.push(wave1);
@@ -52,7 +53,7 @@ function draw() {
       stroke(color1);
       push();
       rotate(history[i]/50);
-      rect(-history[i]*3/2,-history[i]*3/2,2*history[i]*3/2,2*history[i]*3/2);
+      rect(-history[i]*height/400,-history[i]*height/400,2*history[i]*height/400,2*history[i]*height/400);
       pop();
     }
 
@@ -61,7 +62,7 @@ function draw() {
     rotate(wave1/50+PI/4);
     color1.setAlpha(255)
     stroke(color1);
-    rect(-wave1*3/2,-wave1*3/2,2*wave1*3/2,2*3/2*wave1)
+    rect(-wave1*height/400,-wave1*height/400,2*wave1*height/400,2*height/400*wave1)
     pop();
 
     history5.push(wave1);
@@ -75,7 +76,7 @@ function draw() {
         stroke(color1);
         push();
         rotate(history5[i]/50+PI/4);
-        rect(-history5[i]*3/2,-history5[i]*3/2,2*history5[i]*3/2,2*history5[i]*3/2);
+        rect(-history5[i]*height/400,-history5[i]*height/400,2*history5[i]*height/400,2*history5[i]*height/400);
         pop();
       }
 
@@ -84,7 +85,7 @@ function draw() {
     rotate(wave/50);
     color1.setAlpha(255)
     stroke(color1);
-    line(0,-2.5*wave,0,2.5*wave);
+    line(0,-height/240*wave,0,height/240*wave);
     pop();
 
     history2.push(wave);
@@ -98,7 +99,7 @@ function draw() {
       stroke(color1);
       push();
       rotate(history2[i]/50);
-      line(0,-2.5*history2[i],0,2.5*history2[i])
+      line(0,-height/240*history2[i],0,height/240*history2[i])
       pop();
     }
 
@@ -107,7 +108,7 @@ function draw() {
     rotate(wave/50);
     color1.setAlpha(255)
     stroke(color1);
-    line(-2.5*wave,0,2.5*wave,0);
+    line(-height/240*wave,0,height/240*wave,0);
     pop();
 
     history3.push(wave);
@@ -121,7 +122,7 @@ function draw() {
       stroke(color1);
       push();
       rotate(history3[i]/50);
-      line(-2.5*history3[i],0,2.5*history3[i],0)
+      line(-height/240*history3[i],0,height/240*history3[i],0)
       pop();
     }
 
@@ -130,7 +131,7 @@ function draw() {
     rotate(wave/50);
     color1.setAlpha(255)
     stroke(color1);
-    ellipse(0,0,8*wave);
+    ellipse(0,0,height/72*wave);
     pop();
 
     history4.push(wave);
@@ -144,7 +145,7 @@ function draw() {
       stroke(color1);
       push();
       rotate(history4[i]/50);
-      ellipse(0,0,history4[i]*8)
+      ellipse(0,0,history4[i]*height/72)
       pop();
     }
 
@@ -153,6 +154,6 @@ function draw() {
     for(let i=0; i<100; i++){
     rotate(rAngle);
     stroke(169,169,169,125);
-    line(300, i-w, -300, (i+1));
+    line(height*0.52, (i-w)*height/578, -height*0.52, (i+1)*height/578);
   }
 }
